8d20142e00d52ed5dbe06293f0bc871924b6b8ac
medjik <med.akhmetov@gmail.com>
2023-06-29 14:56:29 +0600

    seven attempt

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.module3.task2;
 public class NamingConvention {
     public static void main(String[] args) {
         int variableTen = 10;
-        int variable_one = 1;
+        int variableOne = 1;
         int variableTwo = 2;
         int variable_three = 3;
         final int VARIABLE_FOUR = 4;
