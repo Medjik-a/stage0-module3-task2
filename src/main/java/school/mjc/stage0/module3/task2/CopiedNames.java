1191b2358628e99b79f30c54eb023290085a544e
medjik <med.akhmetov@gmail.com>
2023-06-29 14:24:12 +0600

    second attempt

--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -2,10 +2,17 @@ package school.mjc.stage0.module3.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 2000;
+        int variableOne = 0;
+        int variableTwo = 2000;
         int value = 1000;
+        int variableThree = 4000;
         int value2 = 3000;
         int variables = 5000;
         variables = 7000;
+        int valueTwo = 90000;
+        int valueThree = 100000;
+        int myInteger = 1000000;
+        int myVar = 1234567;
+        int myIntegerSecond = 1234567;
     }
 }
