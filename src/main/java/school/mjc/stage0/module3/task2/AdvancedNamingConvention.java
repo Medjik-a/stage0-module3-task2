697dc5c98cca543644f732cb93e11e3e24a4a94f
medjik <med.akhmetov@gmail.com>
2023-06-29 14:44:32 +0600

    fifth attempt

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -1,20 +1,19 @@
 package school.mjc.stage0.module3.task2;
 
 public class AdvancedNamingConvention {
-    private static final int ADULT_AGE = 18;
-
-    private int age;
-    private int phoneNumber;
-
-    void callToFriend() {
-        //some code
-    }
-
-    void callByNumber(int number) {
-        //some code
-    }
-
     public static void main(String[] args) {
+        private static final int ADULT_AGE = 18;
+
+        private int age;
+        private int phoneNumber;
+
+        void callToFriend() {
+            //some code
+        }
+
+        void callByNumber(int number) {
+            //some code
+        }
 
     }
 }
